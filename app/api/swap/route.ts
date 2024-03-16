2a21beadf5542b63d8016b7afb67508a2eefbdc3
Prawin CHETTIAR <prawin.chettiar@sceme.io>
2024-03-16 13:55:23 +0100

    swap minor update

--- app/api/swap/route.ts
+++ app/api/swap/route.ts
@@ -1,18 +1,39 @@
 import  ethers  from  'ethers';
 import Web3 from 'web3';
+import { privateKeyToSafeSmartAccount } from 'permissionless/accounts';
+import { Address, Hash, createPublicClient, http } from 'viem';
+import { bundlerActions, createSmartAccountClient } from 'permissionless';
+import { pimlicoBundlerActions } from 'permissionless/actions/pimlico';
+import { createPimlicoPaymasterClient } from 'permissionless/clients/pimlico';
+import { sepolia } from 'viem/chains';
+import { abi } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json';
 
-const UNISWAP_ROUTER_ADDRESS = '0x198EF79F1F515F02dFE9e3115eD9fC07183f02fC ';
+const UNISWAP_ROUTER_ADDRESS = '0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E	';
 const ETH_ADDRESS = '0x0000000000000000000000000000000000000000'; // Wrapped ETH address
-const PEPE_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI token address
-const INFURA_API_KEY = '';
+const PEPE_ADDRESS = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238'; // DAI token address
 
-const { abi } = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json');
+const INFURA_API_KEY = process.env.INFURA_API_KEY!;
+const eprivateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY!;
+const privateKey = '0x' + eprivateKey;
+const apiKey = process.env.NEXT_PUBLIC_PIMLICO_API_KEY!;
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`));
+const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+const bundlerUrl = `https://api.pimlico.io/v1/sepolia/rpc?apikey=${apiKey}`
+const nodeUrl = `https://mainnet.infura.io/v3/${INFURA_API_KEY}`
 
-async function createUniswapTransaction(web3: Web3, address: String, amountInETH: Number, amountOutMin: Number) {;
+const publicClient = createPublicClient({
+	transport: http("https://rpc.ankr.com/eth_sepolia"),
+})
+ 
+const paymasterClient = createPimlicoPaymasterClient({
+	transport: http(paymasterUrl),
+})
+
+const web3 = new Web3(new Web3.providers.HttpProvider(nodeUrl));
+
+async function createUniswapTransaction(web3: Web3, address: Address, amountInETH: number, amountOutMin: number) {;
     const uniswapRouter = new web3.eth.Contract(abi, UNISWAP_ROUTER_ADDRESS);
-    const amountIn = ethers.utils.parseEther(amountInETH.toString());
+    const amountIn = ethers.parseEther(amountInETH.toString());
     const path = [ETH_ADDRESS, PEPE_ADDRESS];
     const deadline = Math.floor(Date.now() / 1000) + 60 * 20; 
 
@@ -27,27 +48,54 @@ async function createUniswapTransaction(web3: Web3, address: String, amountInETH
     return data;
 }
 
-async function createTx(web3, account, method_data){
-    var data = method_data.encodeABI();
+async function sendTx(amountInETH: number, amountOutMin: number){
+    const fid = process.env.NEXT_PUBLIC_MYFID;
+    if (!fid) {
+        return null;
+    }
+    const account = await privateKeyToSafeSmartAccount(publicClient, {
+        privateKey: privateKey as Hash,
+        safeVersion: "1.4.1", // simple version
+        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
+        saltNonce: BigInt(fid)
+    })
 
-    const gasPrice = await web3.eth.getGasPrice();
-    const gasEstimate = await method_data.estimateGas({ from: account.address });
+    const data_raw = await createUniswapTransaction(web3, account.address, amountInETH, amountOutMin);
+    const data = data_raw.encodeABI();
 
-    const transaction = {
-        to: MarketplaceAddress,
-        data: data,
-        gasPrice: gasPrice,
-        gas: gasEstimate,
-    };
+    const callData = await account.encodeCallData({ 
+        to: UNISWAP_ROUTER_ADDRESS, 
+        data: `0x${data}`,
+        value: BigInt(amountInETH) 
+    });
 
-    return transaction;
+    const smartAccountClient = createSmartAccountClient({
+        account,
+        chain: sepolia,
+        transport: http(bundlerUrl),
+        sponsorUserOperation: paymasterClient.sponsorUserOperation,
+    })
+        .extend(bundlerActions)
+        .extend(pimlicoBundlerActions)
+
+    const userOperation = await smartAccountClient.prepareUserOperationRequest({
+        userOperation: {
+            callData
+        },
+    })
+
+    userOperation.signature = await account.signUserOperation(userOperation)
+
+    const userOpHash = await smartAccountClient.sendUserOperation({
+        userOperation,
+        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+    })
+
+    return userOpHash;
 }
 
-async function swapMemeCoinForETH(web3, account, amountInETH, amountOutMin) {
-    const tx_data = await createUniswapTransaction(web3, amountInETH, account, amountOutMin);
-    const tx = await createTx(web3, account, tx_data);
-    return tx;
-}
+sendTx(0.1, 0.00001);
 
-module.exports = { swapMemeCoinForETH };
+
+
 
