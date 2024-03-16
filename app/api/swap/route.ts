f7dff30cb6c8393e0999c507f47966c8b7b407a9
Prawin CHETTIAR <prawin.chettiar@sceme.io>
2024-03-16 15:17:02 +0100

    change abi swap

--- app/api/swap/route.ts
+++ app/api/swap/route.ts
@@ -6,7 +6,8 @@ import { bundlerActions, createSmartAccountClient } from 'permissionless';
 import { pimlicoBundlerActions } from 'permissionless/actions/pimlico';
 import { createPimlicoPaymasterClient } from 'permissionless/clients/pimlico';
 import { sepolia } from 'viem/chains';
-import { abi } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json';
+//import { abi } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json';
+import { abi } from '@uniswap/swap-router-contracts/artifacts/contracts/interfaces/ISwapRouter02.sol/ISwapRouter02.json'
 
 const UNISWAP_ROUTER_ADDRESS = '0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E';
 const ETH_ADDRESS = '0x0000000000000000000000000000000000000000'; // Wrapped ETH address
@@ -39,7 +40,7 @@ async function createUniswapTransaction(web3: Web3, address: Address, amountInET
     const path = [ETH_ADDRESS, PEPE_ADDRESS];
     const deadline = Math.floor(Date.now() / 1000) + 60 * 20; 
 
-    const data = await uniswapRouter.methods.swapExactETHForTokens(
+    const data = await uniswapRouter.methods.swapExactTokensForTokens(
         amountOut,
         path,
         address,
@@ -55,13 +56,14 @@ async function sendTx(amountInETH: number, amountOutMin: number){
     if (!fid) {
         return null;
     }
+    console.log("1");
     const account = await privateKeyToSafeSmartAccount(publicClient, {
         privateKey: privateKey as Hash,
         safeVersion: "1.4.1", // simple version
         entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
         saltNonce: BigInt(fid)
     })
-
+    console.log("2");
     const data_raw = await createUniswapTransaction(web3, account.address, amountInETH, amountOutMin);
     const data = data_raw.encodeABI();
 
@@ -70,6 +72,8 @@ async function sendTx(amountInETH: number, amountOutMin: number){
         data: `0x${data}`,
         value: ethers.parseEther(amountInETH.toString())
     });
+
+    console.log("3");
 
     const smartAccountClient = createSmartAccountClient({
         account,
@@ -80,13 +84,18 @@ async function sendTx(amountInETH: number, amountOutMin: number){
         .extend(bundlerActions)
         .extend(pimlicoBundlerActions)
 
+    console.log("4");
+
     const userOperation = await smartAccountClient.prepareUserOperationRequest({
         userOperation: {
             callData
         },
     })
 
+    console.log("5");
+
     userOperation.signature = await account.signUserOperation(userOperation)
+    console.log("6");
 
     const userOpHash = await smartAccountClient.sendUserOperation({
         userOperation,
